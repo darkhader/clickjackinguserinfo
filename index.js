3d9eb377d3fe946b7da6dba0df392b3a14886e3d
unknown <dr.hiep98@gmail.com>
2019-12-22 13:25:03 +0700

    vai cau noi co khien nguoi thay doi

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 const app = express();
 const mongoose = require('mongoose');
-const cors = require('cors');
 const session = require('express-session')
 const userRouter = require('./routers/userRouter');
 
